4bb88daef541809f73e8c90e3d943d164f8fa19d
kennobith <97459261+kennobith@users.noreply.github.com>
2025-08-12 11:03:28 -0300

    Update ExtraExtraApp.py

--- extraextraapp/ExtraExtraApp.py
+++ extraextraapp/ExtraExtraApp.py
@@ -29,6 +29,7 @@ def procesar_novedades_sistema(datos):
                         'valor_hora_extra': datos['valor_hora_extra'],
                     }
     )
+
     df_s.replace(to_replace=['@HRSEXTR1','@HRSEXTR2','@HRSEXTR3'],value=[1,2,3],inplace=True)
 
     df_sistema = pd.pivot_table(
@@ -47,6 +48,7 @@ def procesar_novedades_sistema(datos):
     df_sistema['valor_hora_extra_2'] = pd.to_numeric(df_sistema['valor_hora_extra_2'],downcast='integer')
     df_sistema['valor_hora_extra_3'] = pd.to_numeric(df_sistema['valor_hora_extra_3'],downcast='integer')
     df_sistema = df_sistema.sort_values(by = 'legajo')
+
 
     # lo convertimos a un dict
     resultados_sistema = df_sistema.set_index('legajo').T.to_dict('list')
@@ -113,48 +115,57 @@ def procesar_csvs_oficinas(archivos):
 
     return resultados_reporte
 
-def comparar_y_armar_df(resultados_sistema,resultados_reporte):
+def expand_column(col, prefix):
+        return pd.DataFrame(col.tolist(), 
+                            index=col.index, 
+                            columns=[f"Nombre en {prefix}", f"Oficina en {prefix}", f"H.E. normales en {prefix}", f"H.E. al 50 en {prefix}", f"H.E. al 100 en {prefix}"])
 
+def no_fue_reportado_y_esta_en_oficinas(resultados_sistema,legajo,oficinas):
+    return resultados_sistema[legajo][1] in oficinas
+
+def comparar_y_armar_df(resultados_sistema,resultados_reporte,oficinas):
+
+    no_reportados = []
     no_coinciden = {} #legajos de quienes no coinciden lo reportado y lo cargado en sistema
     no_estan_en_sistema = [] #legajos de quienes fueron reportados pero no cargados en sistema
-    coinciden = {}
 
     # está en reporte pero no en sistema:
     for legajo in resultados_reporte.keys():
         if legajo in resultados_sistema.keys():
             continue
         else:
-            no_estan_en_sistema.append(f'{legajo} - {resultados_reporte[legajo][0]}')
+            no_estan_en_sistema.append(f'Legajo: {legajo} - Oficina: {resultados_reporte[legajo][1]}')
 
     # hacer comparacion
     for legajo in resultados_sistema.keys():
-        # si el legajo no está en reporte
-        if legajo not in resultados_reporte.keys():
-            continue
-            # si el legajo está en reporte
-        else:
-            # si coinciden
+        # si el legajo está en reporte
+        if legajo in resultados_reporte.keys():
             if resultados_sistema[legajo][2:5] == resultados_reporte[legajo][2:5]:
                 continue
             # si no coinciden
             else:
                 no_coinciden[legajo] = {'sistema':resultados_sistema[legajo],'reporte': resultados_reporte[legajo]}
+        # si no esta en el reporte
+        else:
+            #si esta en las oficinas dadas
+            if no_fue_reportado_y_esta_en_oficinas(resultados_sistema,legajo,oficinas):
+                no_reportados.append(f'Legajo: {legajo} - Oficina: {resultados_sistema['legajo'][1]}')
 
     df = pd.DataFrame(no_coinciden.values(),index=no_coinciden.keys())
 
+    if df.empty:
+        return None,no_estan_en_sistema,no_reportados
+    else:
+        df_sistema_expandido = expand_column(df['sistema'], 'sistema')
+        df_reporte_expandido = expand_column(df['reporte'], 'reporte')
 
-    def expand_column(col, prefix):
-        return pd.DataFrame(col.tolist(), 
-                            index=col.index, 
-                            columns=[f"Nombre en {prefix}", f"Oficina en {prefix}", f"H.E. normales en {prefix}", f"H.E. al 50 en {prefix}", f"H.E. al 100 en {prefix}"])
-
-    df_sistema_expandido = expand_column(df["sistema"], "sistema")
-    df_reporte_expandido = expand_column(df["reporte"], "reporte")
-
-    df_final = pd.concat([df_sistema_expandido, df_reporte_expandido], axis=1)
-    return df_final,no_estan_en_sistema
+        df_final = pd.concat([df_sistema_expandido, df_reporte_expandido], axis=1)
+        return df_final,no_estan_en_sistema,no_reportados
 
 #################################
+def procesar_oficinas(oficinas):
+    oficinas = oficinas.split("\n")
+    return [ofi.strip() for ofi in oficinas]
 
 def imprimir_lista(lista):
     s = ''
@@ -179,10 +190,17 @@ with st.expander('Paso 1️⃣: Descargá el archivo de novedades'):
 
 archivos = None
 novedades = None
-
+oficinas = None
 with st.expander('Paso 2️⃣: Subí todos los archivos, tanto los csvs como el de novedades descargado del sistema'):
 
     archivos = st.file_uploader('Subí aca abajo los archivos arrastrando o seleccionando en \'Browse files\'',accept_multiple_files=True)
+
+    oficinas = st.text_area(
+        "Escribí las oficinas en una lista, es decir en cada línea va un número de oficina, "
+        "todas las que incluyan los csvs a procesar. \n Cuando termines, presioná Ctrl + Enter"
+    )
+
+oficinas = procesar_oficinas(oficinas)
 
 with st.expander('Paso 3️⃣: Procesar los datos y ver los resultados'):
 
@@ -196,35 +214,50 @@ with st.expander('Paso 3️⃣: Procesar los datos y ver los resultados'):
                     break
 
             if novedades is None:
-                st.error('No subiste el archivo de novedades, hacelo en el paso 2', icon = '🚨')
+                st.error('No subiste el archivo de novedades, hacelo en el paso 2.', icon = '🚨')
+            if oficinas is None:
+                st.error('No escribiste ninguna oficina, hacelo en el paso 2.', icon = '🚨')
             else:
                 resultados_sistema = procesar_novedades_sistema(novedades)
                 
                 resultados_reporte = procesar_csvs_oficinas(archivos)
                 
-                df,no_estan_en_sistema = comparar_y_armar_df(resultados_sistema,resultados_reporte)
+                df,no_estan_en_sistema,no_reportados = comparar_y_armar_df(resultados_sistema,resultados_reporte,oficinas)
 
-                buffer = io.BytesIO()
-                with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
-                    df.to_excel(writer, sheet_name='incongruencias_hrs_extra')
-                buffer.seek(0)
                 with st.expander('Ver resultados'):
-                    st.dataframe(df)
-
                     if len(no_estan_en_sistema) > 0:
-                        st.write("Estos legajos fueron reportados pero no cargados en el sistema")
+                        st.write("1) Estos legajos fueron reportados pero no cargados en el sistema.")
                         with st.expander("Ver mas"):
                             imprimir_lista(no_estan_en_sistema)
                     else: 
-                        st.write("Todos los legajos reportados a asistencias están cargados en el sistema")
+                        st.write("1) Todos los legajos reportados están cargados al sistema.")
+                    
+                    if len(no_reportados) > 0:
+                        st.write("2) Estos legajos no fueron reportados por las oficinas pero están cargados en el sistema.")
+                        with st.expander("Ver mas"):
+                            imprimir_lista(no_reportados)
+                    else: 
+                        st.write("2) Todos los legajos de las oficinas dadas están reportados.")
+
+                    buffer = io.BytesIO()
+                    if df is not None and not df.empty:
+                        st.write("3) Se encontraron las siguientes inconsistencias:")
+                        st.write(df)
+                        with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+                            df.to_excel(writer, sheet_name='inconsistencias_hrs_extra', index=False)
+                        buffer.seek(0)
+                        st.download_button(
+                            label="Descargar resultados",
+                            data=buffer,
+                            file_name="inconsistencias_hrs_extra.xlsx",
+                            mime="application/vnd.ms-excel",
+                            icon=":material/download:",
+                        )
+
+                    else:
+                        st.write("3) No se encontraron inconsistencias entre lo reportado y el sistema.")
+
                         
-                    st.download_button(
-                        label="Descargar resultados",
-                        data=buffer,
-                        file_name="incongruencias_hrs_extra.xlsx",
-                        mime="application/vnd.ms-excel",
-                        icon=":material/download:",
-                    )
     
 hvar = """
     <script>
